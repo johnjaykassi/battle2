439764f16966397dc27a15a8aa7543cb3bc4cab6
john-jay <kassimayanjide@gmail.com>
2023-09-30 13:56:08 +0000

    new

--- storage.js
+++ storage.js
@@ -1,29 +1,34 @@
 
 
-            document.getElementById("inscriptionForm").addEventListener("submit", function (e) {
-            e.preventDefault();
-            const nomUtilisateur = document.getElementById("nomUtilisateur").value;
-            const motDePasse = document.getElementById("motDePasse").value;
+            document.getElementById("inscriptionForm").addEventListener("submit", function (e) 
+            {
+                e.preventDefault();
+                const nomUtilisateur = document.getElementById("nomUtilisateur").value;
+                const motDePasse = document.getElementById("motDePasse").value;
+                
+                // Enregistrement des données d'inscription dans localStorage
+                localStorage.setItem(nomUtilisateur, motDePasse);
+
+                document.getElementById("message").innerText = "Inscription réussie. Vous pouvez maintenant vous connecter.";
+            });
+
+
+            let connex = document.querySelector("connexionForm");
+            console.log(connex);
+            connex.addEventListener("submit",  (e) => 
+            {
+                e.preventDefault();
+                const nomUtilisateurConnexion = document.getElementById("nomUtilisateurConnexion").value;
+                const motDePasseConnexion = document.getElementById("motDePasseConnexion").value;
+
             
-            // Enregistrement des données d'inscription dans localStorage
-            localStorage.setItem(nomUtilisateur, motDePasse);
+                const motDePasseStocke = localStorage.getItem(nomUtilisateurConnexion);
 
-            document.getElementById("message").innerText = "Inscription réussie. Vous pouvez maintenant vous connecter.";
-        });
-
-
-            document.getElementById("connexionForm").addEventListener("submit", function (e) {
-            e.preventDefault();
-            const nomUtilisateurConnexion = document.getElementById("nomUtilisateurConnexion").value;
-            const motDePasseConnexion = document.getElementById("motDePasseConnexion").value;
-
-            
-            const motDePasseStocke = localStorage.getItem(nomUtilisateurConnexion);
-
-            if (motDePasseStocke === motDePasseConnexion) {
-                document.getElementById("message").innerText = "Connexion réussie !";
-            } else {
-                document.getElementById("message").innerText = "Nom d'utilisateur ou mot de passe incorrect.";
-            }
-        });
+                if (motDePasseStocke === motDePasseConnexion) {
+                    document.getElementById("message").innerText = "Connexion réussie !";
+                } else 
+                {
+                    document.getElementById("message").innerText = "Nom d'utilisateur ou mot de passe incorrect.";
+                }
+            });
     
